b0d98982bf7bfcf689a3cb82ac1b36049e3f9482
Kshitij Chauhan <ckshitij2@gmail.com>
2025-07-06 23:18:12 +0530

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,17 +1,13 @@
-# Binary search
-
-from array import *
+# Bubble sort using array module
+from array import array
 
 a = array('i', [118, 4, 53, 74, 88, 35, 23, 66, 786, 4234, 23, 65645, 22, 1])
 x = 0
 
-for z in range(len(a)-1):
-    for i in range(len(a)-1):
-        if a[i] > a[i+1]:
-            a[i] = a[i]+a[i+1]
-            a[i+1] = a[i]-a[i+1]
-            a[i] = a[i]-a[i+1]
-
+for z in range(len(a) - 1):
+    for i in range(len(a) - 1):
+        if a[i] > a[i + 1]:
+            a[i], a[i + 1] = a[i + 1], a[i]
 
 for i in range(len(a)):
     print(a[i], end=' ')
